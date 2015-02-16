25dab2ed7b58aed44da9e7ce2f39ba70efbb8822
Allen Tran <fakeallentran@gmail.com>
2015-02-16 17:17:17 +1100

    overhaul

---
+++ ppca/_ppca.py
@@ -0,0 +1,127 @@
+import os
+
+import numpy as np
+from scipy.linalg import orth
+import pandas as pd
+
+class PPCA():
+
+    def __init__(self, data):
+
+        self.raw = data
+        self.raw[np.isinf(self.raw)] = np.max(self.raw[np.isfinite(self.raw)])
+        self.data = None
+
+    def fit(self, d=None, tol=1e-4, min_obs=10, verbose=False):
+
+        valid_series = np.sum(~np.isnan(self.raw), axis=0) >= min_obs
+
+        data = self.raw[:, valid_series].copy()
+        N = data.shape[0]
+        D = data.shape[1]
+
+        M = np.nanmean(data, axis=0)
+        stds = np.nanstd(data, axis=0)
+        data = (data - np.tile(M, (N, 1))) / np.tile(stds, (N, 1))
+        observed = ~np.isnan(data)
+        missing = np.sum(~observed)
+        data[~observed] = 0
+
+        # initial
+
+        if d is None:
+            d = round(0.2*D)
+
+        C = np.random.randn(D, d)
+        CC = np.dot(C.T, C)
+        X = np.dot(np.dot(data, C), np.linalg.inv(CC))
+        recon = np.dot(X, C.T)
+        recon[~observed] = 0
+        ss = np.sum((recon-data)**2)/(N*D - missing)
+
+        v0 = np.inf
+        counter = 0
+
+        while True:
+
+            Sx = np.linalg.inv(np.eye(d) + CC/ss)
+
+            # e-step
+            ss0 = ss
+            if missing > 0:
+                proj = np.dot(X, C.T)
+                data[~observed] = proj[~observed]
+            X = np.dot(np.dot(data, C), Sx) / ss
+
+            # m-step
+            XX = np.dot(X.T, X)
+            C = np.dot(np.dot(data.T, X), np.linalg.pinv(XX + N*Sx))
+            CC = np.dot(C.T, C)
+            recon = np.dot(X, C.T)
+            recon[~observed] = 0
+            ss = (np.sum((recon-data)**2) + N*np.sum(CC*Sx) + missing*ss0)/(N*D)
+
+            # calc diff for convergence
+            det = np.log(np.linalg.det(Sx))
+            if np.isinf(det):
+                det = abs(np.linalg.slogdet(Sx)[1])
+            v1 = N*(D*np.log(ss) + np.trace(Sx) - det) \
+                + np.trace(XX) - missing*np.log(ss0)
+            diff = abs(v1/v0 - 1)
+            if verbose:
+                print diff
+            if (diff < tol) and (counter > 5):
+                break
+
+            counter += 1
+            v0 = v1
+
+        C = orth(C)
+        vals, vecs = np.linalg.eig(np.cov(np.dot(data, C).T))
+        order = np.flipud(np.argsort(vals))
+        vecs = vecs[:, order]
+        vals = vals[order]
+
+        C = np.dot(C, vecs)
+        X = np.dot(data, C)
+        
+        # add back scaling
+#data = (data + np.tile(M, (N, 1))) * np.tile(stds, (N, 1))
+
+        # attach objects to class
+        self.C = C
+        self.ss = ss
+        self.M = M
+        self.sigma = stds
+        self.X = X
+        self.data = data
+        self.eig_vals = vals
+        self._calc_var()
+
+    def _calc_var(self):
+
+        if self.data is None:
+            print 'Fit the data model first.'
+            return None
+
+        data = self.data.T
+
+        # variance calc
+        var = np.nanvar(data, axis=1)
+        total_var = var.sum()
+        self.var_exp = self.eig_vals.cumsum() / total_var
+
+    def fit_variance(self, var=0.9, verbose=False):
+
+        d = round(0.9*var*self.raw.shape[1])
+        while True:
+            try:
+                self.fit(d=d,verbose=verbose)
+            except numpy.linalg.linalg.LinAlgError:
+                d *= 0.9
+            else:
+                break
+        self._calc_var()
+        sufficient = np.where(self.var_exp>=var)
+        if sufficient[0].size > 0:
+            return sufficient[0][0]
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 if __name__ == '__main__':
-  name = 'pca_missing'
+  name = 'ppca'
   setup(
     name         = name,
     version      = "0.0.0",
     author       = 'Allen Tran',
-    author_email = 'fakeallentran@gmail.com',
-    description  = 'Probabilistic PCA for replacing missing values',
+    author_email = 'realallentran@gmail.com',
+    description  = 'Probabilistic PCA',
     packages     = find_packages(),
     classifiers  = [
       'Development Status :: 4 - Beta',
@@ -18,4 +18,10 @@ if __name__ == '__main__':
     setup_requires = [
       'setuptools>=3.4.4',
     ],
+    install_requires = [
+      'numpy',
+      'pandas',
+    ],
+
+
   )
